38ace98a4cf22fda47b35b30e1ae45d1888c2acd
DyPeterson <dylan.peterson17@gmail.com>
2022-04-22 09:20:26 -0700

    add second query to sql

--- sql/ch2.sql
+++ sql/ch2.sql
@@ -9,3 +9,13 @@ WHERE
 	name REGEXP 'Hair'
 ORDER BY
 	part_num DESC;
+-- From the sets table, select all the sets that included the word 'Showdown' in the name between 1990 and 2015
+SELECT
+	set_num,
+	name,
+    year
+FROM
+	sets
+WHERE
+	name REGEXP 'Showdown' AND
+    year REGEXP '(199[0-9]|20[0-1][0-5])';
